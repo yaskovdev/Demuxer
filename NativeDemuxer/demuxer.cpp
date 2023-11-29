b040c7dfe38baf91677461aed40ad03c28ad35a3
Sergei Iaskov <sergeiiaskov@microsoft.com>
2023-11-29 18:29:49 +0200

    Cleanup.

--- NativeDemuxer/demuxer.cpp
+++ NativeDemuxer/demuxer.cpp
@@ -9,16 +9,9 @@ extern "C" {
 #include "libavcodec/avcodec.h"
 }
 
-struct sample_fmt_entry
-{
-    AVSampleFormat sample_fmt;
-    const char *fmt_be, *fmt_le;
-};
-
 demuxer::demuxer(): initialized_(false), fmt_ctx_(nullptr), source_buffer_({nullptr, 0}), frame_(nullptr), pkt_(nullptr), video_stream_(nullptr), audio_stream_(nullptr),
-    video_dst_file_(nullptr), audio_dst_file_(nullptr), width_(0), height_(0), pix_fmt_(AV_PIX_FMT_NONE), video_dst_bufsize_(0), video_dst_data_{}, video_dst_linesize_{},
-    audio_stream_idx_(-1), video_stream_idx_(-1), audio_dec_ctx_(nullptr), video_dec_ctx_(nullptr),
-    audio_dst_name_(R"(c:\dev\experiment3\capture.audio)"), video_dst_name_(R"(c:\dev\experiment3\capture.video)"), decoder_needs_packet_(true), current_stream_index_(-1)
+    width_(0), height_(0), pix_fmt_(AV_PIX_FMT_NONE), video_dst_bufsize_(0), video_dst_data_{}, video_dst_linesize_{},
+    audio_stream_idx_(-1), video_stream_idx_(-1), audio_dec_ctx_(nullptr), video_dec_ctx_(nullptr), decoder_needs_packet_(true), current_stream_index_(-1)
 {
     std::cout << "Demuxer created" << "\n";
 }
@@ -76,12 +69,6 @@ int demuxer::initialize()
     {
         video_stream_ = fmt_ctx_->streams[video_stream_idx_];
 
-        if (fopen_s(&video_dst_file_, video_dst_name_, "wb"))
-        {
-            std::cerr << "Could not open destination file " << video_dst_name_ << "\n";
-            return -1;
-        }
-
         /* allocate image where the decoded image will be put */
         width_ = video_dec_ctx_->width;
         height_ = video_dec_ctx_->height;
@@ -98,12 +85,6 @@ int demuxer::initialize()
     if (open_codec_context(&audio_stream_idx_, &audio_dec_ctx_, fmt_ctx_, AVMEDIA_TYPE_AUDIO) >= 0)
     {
         audio_stream_ = fmt_ctx_->streams[audio_stream_idx_];
-
-        if (fopen_s(&audio_dst_file_, audio_dst_name_, "wb"))
-        {
-            fprintf(stderr, "Could not open destination file %s\n", audio_dst_name_);
-            return -1;
-        }
     }
 
     av_dump_format(fmt_ctx_, 0, nullptr, 0);
@@ -160,8 +141,6 @@ int demuxer::read_frame(uint8_t* decoded_data, int* is_video)
             return -1;
         }
     }
-
-    // ----
 
     while (true)
     {
@@ -236,45 +215,6 @@ int demuxer::read_packet(void* opaque, uint8_t* dst_buffer, const int dst_buffer
     return number_of_bytes_to_copy;
 }
 
-int demuxer::decode_packet(AVCodecContext* dec, const AVPacket* pkt)
-{
-    // submit the packet to the decoder
-    int ret = avcodec_send_packet(dec, pkt);
-    if (ret < 0)
-    {
-        fprintf(stderr, "Error submitting a packet for decoding (%s)\n", "");
-        return ret;
-    }
-
-    // get all the available frames from the decoder
-    while (ret >= 0)
-    {
-        ret = avcodec_receive_frame(dec, frame_);
-        if (ret < 0)
-        {
-            // those two return values are special and mean there is no output
-            // frame available, but there were no errors during decoding
-            if (ret == AVERROR_EOF || ret == AVERROR(EAGAIN))
-                return 0;
-
-            fprintf(stderr, "Error during decoding (%s)\n", "");
-            return ret;
-        }
-
-        // write the frame data to output file
-        if (dec->codec->type == AVMEDIA_TYPE_VIDEO)
-            ret = output_video_frame(frame_, width_, height_, pix_fmt_, video_dst_data_, video_dst_linesize_, video_dst_bufsize_, video_dst_file_);
-        else
-            ret = output_audio_frame(frame_, audio_dst_file_);
-
-        av_frame_unref(frame_);
-        if (ret < 0)
-            return ret;
-    }
-
-    return 0;
-}
-
 int demuxer::open_codec_context(int* stream_idx, AVCodecContext** dec_ctx, AVFormatContext* fmt_ctx, AVMediaType type)
 {
     int ret, stream_index;
@@ -330,33 +270,6 @@ int demuxer::open_codec_context(int* stream_idx, AVCodecContext** dec_ctx, AVFor
     return 0;
 }
 
-int demuxer::get_format_from_sample_fmt(const char** fmt, AVSampleFormat sample_fmt)
-{
-    sample_fmt_entry sample_fmt_entries[] = {
-        {AV_SAMPLE_FMT_U8, "u8", "u8"},
-        {AV_SAMPLE_FMT_S16, "s16be", "s16le"},
-        {AV_SAMPLE_FMT_S32, "s32be", "s32le"},
-        {AV_SAMPLE_FMT_FLT, "f32be", "f32le"},
-        {AV_SAMPLE_FMT_DBL, "f64be", "f64le"},
-    };
-    *fmt = nullptr;
-
-    for (int i = 0; i < FF_ARRAY_ELEMS(sample_fmt_entries); i++)
-    {
-        sample_fmt_entry* entry = &sample_fmt_entries[i];
-        if (sample_fmt == entry->sample_fmt)
-        {
-            *fmt = AV_NE(entry->fmt_be, entry->fmt_le);
-            return 0;
-        }
-    }
-
-    fprintf(stderr,
-        "sample format %s is not supported as output format\n",
-        av_get_sample_fmt_name(sample_fmt));
-    return -1;
-}
-
 AVCodecContext* demuxer::current_context() const
 {
     if (current_stream_index_ == audio_stream_idx_)
@@ -369,39 +282,4 @@ AVCodecContext* demuxer::current_context() const
     }
     // TODO: handle this case properly (should skip such packet)
     exit(1);
-}
-
-int demuxer::output_video_frame(AVFrame* frame, int width, int height, AVPixelFormat pix_fmt, uint8_t* video_dst_data[4], int video_dst_linesize[4], int video_dst_bufsize, FILE* video_dst_file)
-{
-    if (frame->width != width || frame->height != height || frame->format != pix_fmt)
-    {
-        /* To handle this change, one could call av_image_alloc again and
-         * decode the following frames into another rawvideo file. */
-        fprintf(stderr, "Error: Width, height and pixel format have to be "
-            "constant in a rawvideo file, but the width, height or "
-            "pixel format of the input video changed:\n"
-            "old: width = %d, height = %d, format = %s\n"
-            "new: width = %d, height = %d, format = %s\n",
-            width, height, av_get_pix_fmt_name(pix_fmt),
-            frame->width, frame->height,
-            av_get_pix_fmt_name((AVPixelFormat)frame->format));
-        return -1;
-    }
-
-    /* copy decoded frame to destination buffer:
-     * this is required since rawvideo expects non aligned data */
-    av_image_copy2(video_dst_data, video_dst_linesize,
-        frame->data, frame->linesize,
-        pix_fmt, width, height);
-
-    /* write to rawvideo file */
-    fwrite(video_dst_data[0], 1, video_dst_bufsize, video_dst_file);
-    return 0;
-}
-
-int demuxer::output_audio_frame(AVFrame* frame, FILE* audio_dst_file)
-{
-    size_t unpadded_linesize = frame->nb_samples * av_get_bytes_per_sample((AVSampleFormat)frame->format);
-    fwrite(frame->extended_data[0], 1, unpadded_linesize, audio_dst_file);
-    return 0;
 }
--- NativeDemuxer/demuxer.h
+++ NativeDemuxer/demuxer.h
@@ -30,10 +30,8 @@ private:
     buffer_data source_buffer_;
     AVFrame* frame_;
     AVPacket* pkt_;
-    AVStream *video_stream_;
-    AVStream *audio_stream_;
-    FILE* video_dst_file_;
-    FILE* audio_dst_file_;
+    AVStream* video_stream_;
+    AVStream* audio_stream_;
     int width_;
     int height_;
     AVPixelFormat pix_fmt_;
@@ -42,10 +40,8 @@ private:
     int video_dst_linesize_[4];
     int audio_stream_idx_;
     int video_stream_idx_;
-    AVCodecContext *audio_dec_ctx_;
-    AVCodecContext *video_dec_ctx_;
-    const char* audio_dst_name_;
-    const char* video_dst_name_;
+    AVCodecContext* audio_dec_ctx_;
+    AVCodecContext* video_dec_ctx_;
     bool decoder_needs_packet_;
     int current_stream_index_;
 
@@ -53,15 +49,7 @@ private:
 
     AVCodecContext* current_context() const;
 
-    static int output_video_frame(AVFrame* frame, int width, int height, AVPixelFormat pix_fmt, uint8_t* video_dst_data[4], int video_dst_linesize[4], int video_dst_bufsize, FILE* video_dst_file);
-
-    static int output_audio_frame(AVFrame* frame, FILE* audio_dst_file);
-
     static int read_packet(void* opaque, uint8_t* dst_buffer, int dst_buffer_size);
 
-    int decode_packet(AVCodecContext* dec, const AVPacket* pkt);
-
     static int open_codec_context(int* stream_idx, AVCodecContext** dec_ctx, AVFormatContext* fmt_ctx, AVMediaType type);
-
-    static int get_format_from_sample_fmt(const char** fmt, AVSampleFormat sample_fmt);
 };
